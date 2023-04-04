6f47c83efea8a327eae0da83c1aabf0d931b0a33
Honoka-Otani <81026pooh@gmail.com>
2023-04-05 01:13:58 +0900

    add mv-img

--- page-mv.php
+++ page-mv.php
@@ -15,6 +15,9 @@ $query = new WP_Query(
   .openbtn{
     background-color:rgba(255,255,255,0);
   }
+  .mv-img{
+    background-img:url()
+  }
 </style>
 <section class="music-video">
 
@@ -24,7 +27,11 @@ $query = new WP_Query(
     <li class="mv-list">
       <div class="mv-item">
       <?php if (has_post_thumbnail()) : /* もしアイキャッチが登録されていたら */ ?>
-       <?php the_post_thumbnail(); ?>
+        <div class="mv-img">
+        <a href="<?php echo CFS()->get('youtube_url'); ?>">
+        <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="mv-img">
+        </a>
+      </div>
        <?php else: /* 登録されていなかったら */ ?>
         <iframe width="560" height="315" class="lazyload" data-src="<?php echo CFS()->get('youtube_url'); ?>?modestbranding=1&mute=1&?playsinline=1&rel=0" title="YouTube video player"
           frameborder="0"
